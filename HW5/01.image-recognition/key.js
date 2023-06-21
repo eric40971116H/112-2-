168df6e78abb08f845b06154508279732ba0eb38
eric40971116H <40971116H@gapps.ntnu.edu.tw>
2023-06-21 17:24:42 +0800

    06

---
+++ HW5/01.image-recognition/key.js
@@ -0,0 +1,2 @@
+//請填上申請好的API KEY
+const subscriptionKey = "fc30f800dc2b45e0a92fdc6499a3c067";
---
+++ HW5/01.image-recognition/main.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+    //do something
+    $("#thisButton").click(function(){
+        processImage();
+    });
+});
+
+function processImage() {
+    
+    //確認區域與所選擇的相同或使用客製化端點網址
+    var url = "https://eastus.api.cognitive.microsoft.com/";
+    var uriBase = url + "vision/v2.1/analyze";
+    
+    var params = {
+        "visualFeatures": "Objects,ImageType,Faces,Adult,Brands,Categories,Description",
+        "details": "Landmarks",
+        "language": "zh",
+    };
+    //顯示分析的圖片
+    var sourceImageUrl = document.getElementById("inputImage").value;
+    document.querySelector("#sourceImage").src = sourceImageUrl;
+    //送出分析
+    $.ajax({
+        url: uriBase + "?" + $.param(params),
+        // Request header
+        beforeSend: function(xhrObj){
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+        },
+        type: "POST",
+        // Request body
+        data: '{"url": ' + '"' + sourceImageUrl + '"}',
+    })
+    .done(function(data) {
+        //顯示JSON內容
+        $("#responseTextArea").val(JSON.stringify(data, null, 2));
+        $("#picDescription").empty();
+        $("#picDescription").append(data.description.captions[0].text+"<br>");
+        $("#picDescription").append("這品牌是"+data.brands[0].name+"<br>");
+        $("#picDescription").append("這種類是"+data.categories[0].name+"score="+data.categories[0].score+"<br>");
+        $("#picDescription").append("偵測到"+data.objects[0].object+"<br>");
+        $("#picDescription").append("偵測到"+data.objects[1].object+"<br>");
+        
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        //丟出錯誤訊息
+        var errorString = (errorThrown === "") ? "Error. " : errorThrown + " (" + jqXHR.status + "): ";
+        errorString += (jqXHR.responseText === "") ? "" : jQuery.parseJSON(jqXHR.responseText).message;
+        alert(errorString);
+    });
+};
